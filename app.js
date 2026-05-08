deeb2445a3aed44ff2f2620222400356702ad1c4
Riku Matsui <184587145+langue-de77@users.noreply.github.com>
2026-05-09 00:21:35 +0900

    update results
    文字数などの表示方法を変更しました

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ function updateCounter(text) {
     const excludedLFLength = measureExcludedLF(text);
     updateSpecificCounter(input.value, specificInput.value);
     console.log(length, excludedLFLength);
-    counter.innerHTML = `文字数: ${length}`;
-    exCounter.innerHTML = `改行除く: ${excludedLFLength}`;
+    counter.textContent = `${length}`;
+    exCounter.textContent = `${excludedLFLength}`;
 }
 
 function updateSpecificCounter(text, specificText) {
     var count = 0;
     if (specificText.length > 0)
         count = countSpecificText(text, specificText);
-    specificCounter.innerHTML = `特定の文字列の数: ${count}`;
+    specificCounter.textContent = `${count}`;
 }
 
 function measureExcludedLF(text) {
